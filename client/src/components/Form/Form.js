ba334202a95203df9179c6cce5db9beec26c6c24
roangira <angirarobert7@gmail.com>
2022-12-16 08:42:18 +0300

    implementing form logic

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 import useStyles from './styles'
 
 const Form = () => {
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     const classes = useStyles();
     
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+            <Typography variant="h6">Creating a Memory</Typography>
+            <TextField 
+                name="creator" 
+                variant="outlined" 
+                label="Creator" 
+                fullWidth
+                value={postData.creator}
+                onChange={(e) => setPostData({ ...postData, creator: e.target.value })} // allows chnge of a specific property in a textfield while other data persists
+            /> 
+            </form>
+        </Paper>
     );
 }
 
