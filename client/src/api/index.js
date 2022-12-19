ed8d8e3152991c61a5997c1930f47b4ba257b78d
roangira <angirarobert7@gmail.com>
2022-12-19 11:33:57 +0300

    Created a post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios'; // used to make API calls
 const url = 'http://localhost:5000/posts'; // returns all post that we currently have in the database
 
 export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -11,7 +11,7 @@ const postSchema = mongoose.Schema({
         type: Number,
         default: 0
     },
-    createsAt: {
+    createdAt: {
         type: Date,
         default: new Date()
     },
