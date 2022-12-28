260dd6f0e215807daeea79ebacd1e7bebcc3eddf
roangira <angirarobert7@gmil.com>
2022-12-28 09:01:52 +0300

    Implemented like functionality

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,8 +4,9 @@ export default (posts = [], action) => {
     switch (action.type) {
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload);
-        case 'UPDATE':
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post); // if-else statement
+        case 'UPDATE': //both the update and like cases are goint to do the same thing
+        case 'LIKE':
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post)); // if-else statement
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
