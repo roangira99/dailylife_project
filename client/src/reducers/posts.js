810a00f5f2093c8558e95eaa4265b9c6ecf166d8
roangira <angirarobert7@gmil.com>
2022-12-30 08:52:05 +0300

    Made additional fixes and action constants

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,16 @@
 // a reducers is a function that accepts the state and the action then based on the action type, 
 // you can execute some logic
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-        case 'UPDATE': //both the update and like cases are goint to do the same thing
-        case 'LIKE':
+        case UPDATE: //both the update and like cases are goint to do the same thing
             return posts.map((post) => (post._id === action.payload._id ? action.payload : post)); // if-else statement
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         default:
             return posts;
