b5323728c076d7a34f80356e4f347fb8d148a002
roangira99 <angirarobert7@gmail.com>
2023-01-04 11:49:00 +0300

    Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux'; // allows us to dispatch an action
+import { BrowserRouter, Swich, Route } from 'react-router-dom';
 
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import dailylife from './images/dailylife.png';
 import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -19,22 +20,8 @@ const App = () => {
     
     return (
         <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position='static' color="inherit">
-                <Typography className={classes.heading} variant="h2" align='center'>Dailylife</Typography>
-                <img className={classes.image} src={dailylife} alt="dailylife" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+           <Navbar />
+            
         </Container>
     );
 }
